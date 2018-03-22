ab74add4b249f2d01376b310307492dcf0438e89
zedemger <zuhairgoolfee@gmail.com>
2018-03-22 08:43:50 -0700

    updated footer files

--- main files/footer.php
+++ main files/footer.php
@@ -1,35 +1,15 @@
         <footer>
-            <ul>
-                <li><p>Wholesaler</p>
-                    <ul>
-                        <li><p>Retailer</p></li>
-                    </ul>
-                </li>
-                <li><a href="">Categories</a>
-                    <ul>
-                        <li><p>Nike</p></li>
-                        <li><p>Adidas</p></li>
-                        <li><p>Puma</p></li>
-                        <li><p>Fahion</p></li>
-                    </ul>
-                </li>
-                <li><a href="">Help &amp; Contact</a>
-                    <ul>
-                        <li><a href="">FAQ</a></li>
-                        <li><a href="">location</a></li>
-                        <li><p>tel(+230 56 54 15 32)</p></li>
-                        <li><p>phone(+230 56 54 15 32)</p></li>
-                        <li><p>fax(+230 56 54 15 32)</p></li>
-                    </ul>
-                </li>
-                <li><a href="">Stay Connected</a>
-                    <ul>
-                        <li><a href=""><i class="fab fa-facebook"></i> Facebook</a></li>
-                        <li><a href=""><i class="fab fa-instagram"></i> Instagram</a></li>
-                        <li><a href=""><i class="fab fa-twitter"></i> Twitter</a></li>
-                    </ul>
-                </li>
-            </ul>
+            <div class="footer-inner">
+                <div class="footer-left">
+                    <div class="footer-box">
+                        <h3>SSS SHOES</h3>
+                        <ul>
+                            <li><a href="">Wholesaler</a></li>
+                            <li><a href="">Retailer</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </footer>
     </body>
 
