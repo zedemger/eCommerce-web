96e295676bde5e7828ca2f94bd585b8a5fd6f392
zedemger <zuhairgoolfee@gmail.com>
2018-03-21 19:01:39 -0700

    updated home.php and style.css files

---
+++ main files/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>SSS SHOES mauritius - Buy Shoes Online</title>
+        
+        <link rel="stylesheet" href="\css\_common\style.css">
+    </head>
+    <body>
+        <div class="top-nav">
+            <p><a href="home.php">SSS SHOES</a></p>
+            <ul>
+                <li><a href="">Sign in</a></li>
+                <li><a href="">help</a>
+                    <ul>
+                        <li><a href="">Faq</a></li>
+                        <li><a href="">Ask a Question</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </body>
+
+</html>
